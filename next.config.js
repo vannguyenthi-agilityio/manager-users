924801209e0519c273fe1689111a758bace6e73b
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-08-11 00:35:32 +0700

    Update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  distDir: 'build',
-  "pagesDir": './src/pages'
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
+const path = require('path');
 
-module.exports = nextConfig
+// Load env into browser
+const nextEnv = require('next-env');
+const dotenvLoad = require('dotenv-load');
+
+dotenvLoad()
+
+module.exports = withPlugins([[nextEnv()], [withImages]], {
+  async rewrites() {
+    return [
+      // These rewrites are checked after both pages/public files
+      // and dynamic routes are checked
+      {
+        source: '/',
+        destination: '/',
+      }
+    ]
+  },
+  webpack(config, options) {
+    config.resolve.modules.push(path.resolve('./'));
+    return config;
+  },
+});
