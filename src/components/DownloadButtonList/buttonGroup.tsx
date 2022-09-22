ef333eb103c84ffaa4f4132fd33c087da4ecd679
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-09-22 22:56:37 +0700

    feat: try img in svg

--- src/components/DownloadButtonList/buttonGroup.tsx
+++ src/components/DownloadButtonList/buttonGroup.tsx
@@ -5,22 +5,24 @@ export default function ButtonGroup({ className = '' }: OriginalProps) {
   return (
     <div className={`download-button-list ${className}`}>
       <a href="#" className="download-button-item">
-        
+      <svg>
           <img
             src="/assets/images/icons/app-store.svg"
             alt="DogeCard Apple Store"
             width="100%"
             height="56px"
           />
+        </svg>
       </a>
       <a href="#" className="download-button-item">
-        
+        <svg>
           <img
             src="/assets/images/icons/google-play.svg"
             alt="DogeCard Google Play"
             width="100%"
             height="56px"
           />
+        </svg>
       </a>
     </div>
   );
