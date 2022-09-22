79fb899d59cacfe2a3f78451c58361a4bc546cf5
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-09-22 07:58:30 +0700

    feat: update image

--- src/components/DownloadButtonList/buttonGroup.tsx
+++ src/components/DownloadButtonList/buttonGroup.tsx
@@ -5,36 +5,21 @@ export default function ButtonGroup({ className = '' }: OriginalProps) {
   return (
     <div className={`download-button-list ${className}`}>
       <a href="#" className="download-button-item">
-        <picture>
-          <source
-            media="(min-width: 768px)"
-            srcset="/assets/images/icons/app-store.svg"
-            width="192"
-            height="100%"
-          />
           <img
             src="/assets/images/icons/app-store.svg"
             alt="DogeCard Apple Store"
             width="192"
             height="100%"
           />
-        </picture>
       </a>
       <a href="#" className="download-button-item">
-        <picture>
-          <source
-            media="(min-width: 768px)"
-            srcset="/assets/images/icons/google-play.svg"
-            width="192"
-            height="100%"
-          />
+        
           <img
             src="/assets/images/icons/google-play.svg"
             alt="DogeCard Google Play"
             width="192"
             height="100%"
           />
-        </picture>
       </a>
     </div>
   );
