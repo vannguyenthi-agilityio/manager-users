86bbb6244dba44a96a16b443723fd9e85d82ccef
Van Nguyen <van.nguyenthi@asnet.com.vn>
2022-09-22 23:04:14 +0700

    feat: checked video instead img

--- src/components/DownloadButtonList/buttonGroup.tsx
+++ src/components/DownloadButtonList/buttonGroup.tsx
@@ -5,24 +5,26 @@ export default function ButtonGroup({ className = '' }: OriginalProps) {
   return (
     <div className={`download-button-list ${className}`}>
       <a href="#" className="download-button-item">
-      <svg>
+      {/* <svg>
           <img
             src="/assets/images/icons/app-store.svg"
             alt="DogeCard Apple Store"
             width="100%"
             height="56px"
           />
-        </svg>
+        </svg> */}
+        <video poster="/assets/images/icons/app-store.svg"></video>
       </a>
       <a href="#" className="download-button-item">
-        <svg>
+       <video poster="/assets/images/icons/google-play.svg"></video>
+        {/* <svg>
           <img
             src="/assets/images/icons/google-play.svg"
             alt="DogeCard Google Play"
             width="100%"
             height="56px"
           />
-        </svg>
+        </svg> */}
       </a>
     </div>
   );
